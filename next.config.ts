4ec18ff36814934e55350ecaf9381f4f354c7a40
Wahyu Kurniawan <ayungavis@gmail.com>
2024-12-28 19:29:01 +0700

    🔧 chore: remove deprecated legacy warning API

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  sassOptions: {
+    silenceDeprecations: ['legacy-js-api'],
+  },
 };
 
 export default nextConfig;

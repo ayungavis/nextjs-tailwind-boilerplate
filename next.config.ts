29fab1d5aa6fa6b52dc276a0a07796379d6a13bd
Wahyu Kurniawan <ayungavis@gmail.com>
2024-12-28 19:40:51 +0700

    🐛 fix: use `sass-loader` to fix the legacy API warning instead of silence the warning

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,17 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  sassOptions: {
-    silenceDeprecations: ['legacy-js-api'],
+  experimental: {
+    turbo: {
+      rules: {
+        // https://github.com/sass/dart-sass/issues/2352#issuecomment-2360899608
+        // Use modern sass loader instead of deprecated sass legacy js API
+        '*.scss': {
+          loaders: ['sass-loader'],
+          as: '*.css',
+        },
+      },
+    },
   },
 };
 

3e56f80576c13f26b0c058db521f4232c39ab46d
Wahyu Kurniawan <ayungavis@gmail.com>
2025-04-27 18:45:55 +0700

    chore: ugrade to tailwind v4

--- next.config.ts
+++ next.config.ts
@@ -6,18 +6,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  experimental: {
-    turbo: {
-      rules: {
-        // https://github.com/sass/dart-sass/issues/2352#issuecomment-2360899608
-        // Use modern sass loader instead of deprecated sass legacy js API
-        '*.scss': {
-          loaders: ['sass-loader'],
-          as: '*.css',
-        },
-      },
-    },
-  },
 };
 
 export default nextConfig;
